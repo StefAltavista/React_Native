3819983fcf1b8fd371e3029ca9d4ba93d1e50475
Stef Altavista <stefanomsc@hotmail.it>
2023-01-12 12:52:43 +0100

    resolve dependecies issues

--- track-app/src/api/tracker.js
+++ track-app/src/api/tracker.js
@@ -1,21 +1,21 @@
 import axios from "axios";
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const instance = axios.create({
-  baseURL: "http://b983d225872d.ngrok.io",
+    baseURL: "http://b983d225872d.ngrok.io",
 });
 
 instance.interceptors.request.use(
-  async (config) => {
-    const token = await AsyncStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    async (config) => {
+        const token = await AsyncStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
     }
-    return config;
-  },
-  (err) => {
-    return Promise.reject(err);
-  }
 );
 
 export default instance;
